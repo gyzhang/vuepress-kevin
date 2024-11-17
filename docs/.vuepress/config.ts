13573bb9c1ab9a85ac4833b928f1c03e70dad79c
xprogrammer <xprogrammer@163.com>
2024-11-17 10:38:47 +0800

    配置gitalk

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -295,11 +295,11 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       {
         choosen: 'gitalk',
         options: {
-          clientID: 'a6e1355287947096b88b',
-          clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-          repo: 'blog-gitalk-comment', // GitHub 仓库
-          owner: 'xugaoyi', // GitHub仓库所有者
-          admin: ['xugaoyi'], // 对仓库有写权限的人
+          clientID: 'Ov23liZGAXYw1b9ivaBG',
+          clientSecret: '40a158f5b6379ffdd3ea234551ae346d19d35705',
+          repo: 'gitalk-comment', // GitHub 仓库
+          owner: 'gyzhang', // GitHub仓库所有者
+          admin: ['gyzhang'], // 对仓库有写权限的人
           // distractionFreeMode: true,
           pagerDirection: 'last', // 'first'正序 | 'last'倒序
           id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
